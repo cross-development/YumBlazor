357c5b89dd667f1658d2970dfa92d05938c98dfe
Vitaliy Derda <derdavitaliy@gmail.com>
2025-02-10 18:00:21 +0200

    feat: added upsert page and toastr notifications

---
+++ Extensions/JsRuntimeExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.JSInterop;
+
+namespace YumBlazor.Extensions;
+
+public static class JsRuntimeExtensions
+{
+    public static async Task ToasrtSuccess(this IJSRuntime js, string message)
+    {
+        await js.InvokeVoidAsync("ShowToastr", "success", message);
+    }
+
+    public static async Task ToasrtError(this IJSRuntime js, string message)
+    {
+        await js.InvokeVoidAsync("ShowToastr", "error", message);
+    }
+}
---
+++ wwwroot/js/notification.js
@@ -0,0 +1,15 @@
+﻿window.ShowToastr = function (type, message) {
+    switch (type) {
+        case "success":
+            toastr.success(message);
+            break;
+
+        case "error":
+            toastr.error(message);
+            break;
+
+        default:
+            toastr.info(message);
+            break;
+    }
+}
